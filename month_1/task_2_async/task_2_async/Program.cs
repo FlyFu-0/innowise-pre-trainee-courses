9b960a7f644e4224db0c770f039642754da6d799
Andrew Tihonov <tihonov-04@yandex.ru>
2025-10-04 18:05:32 +0300

    async/await

---
+++ month_1/task_2_async/task_2_async/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace task_2_async;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string[] data = ["Файл 1", "Файл 2", "Файл 3"];
+
+        foreach (var item in data)
+        {
+            Console.WriteLine(ProcessData(item));
+        }
+
+        var tasks = data.Select(ProcessDataAsync).ToArray();
+
+        var results = await Task.WhenAll(tasks);
+        
+        Console.WriteLine();
+        foreach (var item in results)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public static string ProcessData(string dataName)
+    {
+        Thread.Sleep(3000);
+
+        return $"Обработка {dataName} завершена за 3 секунды";
+    }
+
+    public static async Task<string> ProcessDataAsync(string dataName)
+    {
+        await Task.Delay(3000);
+
+        return $"Обработка {dataName} завершена за 3 секунды";
+    }
+}
